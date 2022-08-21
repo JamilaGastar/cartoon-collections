5be373e2618c213663eb1ff044b520d125629472
jamilagastar <jamilagastar@gmail.com>
2022-08-21 15:19:52 +1000

    completed lab

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1).collect {|dwarf, number| puts "#{number}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+
+def summon_captain_planet(planeteer_calls)
+  new_planeteer_calls = planeteer_calls.collect {|call| call.capitalize()}
+  new_planeteer_calls.collect {|call| "#{call}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
+
+def long_planeteer_calls(calls)
+  calls.any?{|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+long_planeteer_calls(["puffs", "go", "two"])
+
+def find_the_cheese(collection_of_cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  collection_of_cheeses.detect {|element|
+ cheese_types.detect{|cheese| element == cheese}}
 end
+
+find_the_cheese(["ham", "apple", "gin"])
